620927f25f4efb7a2d26e70c8ed0445a597eb252
royfuwei <royfuwei@gmail.com>
2023-07-08 21:18:34 +0800

    feat(packages/utility): add delay times utility

---
+++ packages/utility/src/lib/delay.spec.ts
@@ -0,0 +1,23 @@
+import { Delay, delay } from './delay';
+
+it('Test Delay clear', async () => {
+    const delayedPromise = Delay.createDelay()(8000);
+    setTimeout(() => {
+        console.log(`setTimeout 500`);
+        Delay.clearDelay(delayedPromise);
+    }, 500);
+    await delayedPromise;
+});
+
+it('Test Delay clear', async () => {
+    const delayedPromise = delay(8000);
+    setTimeout(() => {
+        console.log(`setTimeout 500`);
+        Delay.clearDelay(delayedPromise);
+    }, 500);
+    await delayedPromise;
+});
+
+it('Test Delay create', async () => {
+    await delay(4000);
+});
