11125f8e6a4ddac8044710b471ff1940f360dc66
royfuwei <royfuwei@gmail.com>
2023-07-12 22:19:45 +0800

    refactor(@rfjs/utils): @nx/workspace move packages @rfjs/utility to @rfjs/utils

--- packages/utils/jest.config.ts
+++ packages/utils/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'utility',
+  displayName: 'utils',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/utility',
+  coverageDirectory: '../../coverage/packages/utils',
 };
