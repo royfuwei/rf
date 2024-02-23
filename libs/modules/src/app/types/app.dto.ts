2b58ebfcae52902949f6086017f60d0e0ef0c68a
royfuwei <royfuwei@gmail.com>
2024-02-23 20:58:48 +0800

    feat(libs/common): add common decorator ApiResSchema

--- libs/modules/src/app/types/app.dto.ts
+++ libs/modules/src/app/types/app.dto.ts
@@ -1,9 +1,21 @@
+import { IsString } from "class-validator";
+import { JSONSchema } from "class-validator-jsonschema";
+
 export interface AppInfo {
   uuid: string;
   environment: string;
 }
 
 export class AppInfoDTO implements AppInfo {
+  @JSONSchema({
+    description: 'uuid desc',
+  })
+  @IsString()
   uuid = '';
+
+  @JSONSchema({
+    description: 'env desc',
+  })
+  @IsString()
   environment = '';
 }
