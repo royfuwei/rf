2970ec2aa658197a8abf01f8b98b40d9fc282070
royfuwei <royfuwei@gmail.com>
2024-05-28 20:14:13 +0800

    ci: add rfjs project's helm deploy flow

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,11 +2,17 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const path = require('path');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
