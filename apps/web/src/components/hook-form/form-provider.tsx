80f319f5090110cc40c2801384411deddea90279
royfuwei <royfuwei@gmail.com>
2024-06-09 15:25:02 +0800

    feat(apps/web): add apps/web's common components hook-form, iconify and logo

---
+++ apps/web/src/components/hook-form/form-provider.tsx
@@ -0,0 +1,19 @@
+import { FormProvider as Form, UseFormReturn } from 'react-hook-form';
+
+type Props = {
+  children: React.ReactNode;
+  methods: UseFormReturn<any>;
+  onSubmit?: VoidFunction;
+};
+
+export default function FormProvider({
+  children,
+  onSubmit,
+  methods,
+}: Readonly<Props>) {
+  return (
+    <Form {...methods}>
+      <form onSubmit={onSubmit}>{children}</form>
+    </Form>
+  );
+}
