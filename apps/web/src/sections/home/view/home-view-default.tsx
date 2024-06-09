213dcfd59929628e8b7b8be68fd2c324dbe9d7b7
royfuwei <royfuwei@gmail.com>
2024-06-09 15:45:42 +0800

    feat(apps/web): adjust apps/web's default home view sections

--- apps/web/src/sections/home/view/home-view-default.tsx
+++ apps/web/src/sections/home/view/home-view-default.tsx
@@ -1,15 +1,16 @@
 'use client';
 
+import styles from './home-view-default.css';
 import { Minimal } from '@rfjs/minimal';
-import styles from './page.module.scss';
+import { Container } from '@mui/material';
 
-export default async function HomeDefaultView() {
+export default function HomeDefaultView() {
   /*
    * Replace the elements below with your own.
    *
    * Note: The corresponding styles are in the ./index.scss file.
    */
-  return (
+  const defaultView = (
     <div className={styles.page}>
       <div className="wrapper">
         <div className="container">
@@ -423,25 +424,9 @@ export default async function HomeDefaultView() {
               </pre>
             </details>
           </div>
-
-          <p id="love">
-            Carefully crafted with
-            <svg
-              fill="currentColor"
-              stroke="none"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-              />
-            </svg>
-          </p>
         </div>
       </div>
     </div>
   );
+  return <Container>{defaultView}</Container>;
 }
--- apps/web/src/sections/home/view/index.ts
+++ apps/web/src/sections/home/view/index.ts
@@ -1,1 +1,2 @@
 export { default as HomeView } from './home-view';
+export { default as HomeDefaultView } from './home-view-default';
