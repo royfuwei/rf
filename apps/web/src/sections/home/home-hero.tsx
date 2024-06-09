20fca0f14da9b41a9d21e1cd59e138acc2e03803
royfuwei <royfuwei@gmail.com>
2024-06-09 15:26:34 +0800

    feat(apps/web): add apps/web's home sections

---
+++ apps/web/src/sections/home/home-hero.tsx
@@ -0,0 +1,25 @@
+import { Box, Stack } from '@mui/material';
+import { Minimal } from '@rfjs/minimal';
+
+export default function HomeHero() {
+  return (
+    <>
+      <Stack
+        alignItems="center"
+        justifyContent="center"
+        sx={{
+          height: 1,
+          mx: 'auto',
+          maxWidth: 480,
+          opacity: 0.7,
+          mt: {
+            // md:
+          },
+        }}
+      >
+        <Minimal id={'welcome'} />
+      </Stack>
+      <Box sx={{ height: { md: '50vh' } }} />
+    </>
+  );
+}
