3cfbbbc51795b45cd3ba8b4d8c2fcab018a89ae4
royfuwei <royfuwei@gmail.com>
2024-09-16 08:36:28 +0800

    feat(apps/api): add simple schedule controller

--- apps/api/src/controllers/index.ts
+++ apps/api/src/controllers/index.ts
@@ -2,3 +2,4 @@ export * from './app.controller';
 export * from './auth.supabase.controller';
 export * from './line.bot.controller';
 export * from './line.notify.controller';
+export * from './schedule.controller';
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,18 @@
 import 'reflect-metadata';
 import { server } from './server';
 import { LoggerHelper } from './common/helpers/logger.helper';
+import { ScheduleRegistry } from '@rfjs/helpers';
+import { initSchedule } from './schedule';
 
 async function main() {
   const { httpServer } = await server();
+  await initSchedule();
 
   const closeProcesses = async (code = 1) => {
     httpServer.close(() => {
       LoggerHelper.log.info('Server closed');
     });
+    ScheduleRegistry.gracefulShutdown();
     process.exit(code);
   };
 
